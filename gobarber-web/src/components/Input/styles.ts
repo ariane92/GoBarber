3d10ca07f1bf001aae88f3ebfaf3e1857a920d82
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-25 13:41:07 -0300

    Tooltip Errors

--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErroed: boolean;
 }
 export const Container = styled.div<ContainerProps>`
   background: #232129;
@@ -21,6 +23,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${(props) =>
+    props.isErroed &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -32,6 +40,7 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
 
   input {
     flex: 1;
@@ -46,5 +55,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #f4ede8;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
