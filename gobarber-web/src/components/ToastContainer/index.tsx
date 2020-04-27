b57219f5e6e60aba923b045a8397be3f542f1067
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-27 18:56:17 -0300

    animate toast

--- gobarber-web/src/components/ToastContainer/index.tsx
+++ gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
+
 import Toast from './Toast';
 import { Container } from './styles';
 import { ToastMessage } from '../../hooks/ToastContext';
@@ -7,10 +9,19 @@ interface ToastContainerProps {
   messages: ToastMessage[];
 }
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-110%' },
+      enter: { right: '0%' },
+      leave: { right: '-110%' },
+    }
+  );
   return (
     <Container>
-      {messages.map((message) => (
-        <Toast key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
       ))}
     </Container>
   );
