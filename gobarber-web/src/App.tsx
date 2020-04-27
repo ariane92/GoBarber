c925cad57a33f1685c37c43d8859b60c345ddd56
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-27 09:42:58 -0300

    Structure of Toast

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignIn from './pages/SignIn/index';
 // import SignUp from './pages/SignUp/index';
+import ToastContainer from './components/ToastContainer';
 import GlobalStyle from './styles/global';
 import { AuthProvider } from './hooks/AuthContext';
 
@@ -9,6 +10,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -38,8 +38,12 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        // toast
       }
     },
     [SignIn]
