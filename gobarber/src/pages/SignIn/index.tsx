52c8d762d5a4f2a51d52998444e0ce7290694c93
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-30 19:14:28 -0300

    authentication screen

--- gobarber/src/pages/SignIn/index.tsx
+++ gobarber/src/pages/SignIn/index.tsx
@@ -1,21 +1,59 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassord,
+  ForgotPassordText,
+  CreateAcccountButtonText,
+  CreateAcccountButton,
+} from './styles';
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu Logon</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu Logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => {}}>Entrar</Button>
+            <ForgotPassord onPress={() => {}}>
+              <ForgotPassordText>Esqueci minha senha</ForgotPassordText>
+            </ForgotPassord>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAcccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAcccountButtonText>Criar uma conta</CreateAcccountButtonText>
+      </CreateAcccountButton>
+    </>
   );
 };
 
