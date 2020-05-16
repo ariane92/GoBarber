4bc5edb56b34d23341581c8112d03dde82a328ff
Ariane Mateus <ariane.ccomp@gmail.com>
2020-05-16 17:44:36 -0300

    feat: :sparkles: template engine

--- gobarber-backend/src/shared/container/providers/MailTemplateprovider/fakes/FakeMailTemplateProvider.ts
+++ gobarber-backend/src/shared/container/providers/MailTemplateprovider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IParseMailTemplateProvider from '../models/IMailTemplateProvider';
 
 export default class FakeMailTemplateProvider
   implements IParseMailTemplateProvider {
-  public async parser({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parser(): Promise<string> {
+    return '';
   }
 }
--- gobarber-backend/src/shared/container/providers/MailTemplateprovider/implementations/HandleBarsMailTemplateProvider.ts
+++ gobarber-backend/src/shared/container/providers/MailTemplateprovider/implementations/HandleBarsMailTemplateProvider.ts
@@ -1,14 +1,18 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IParseMailTemplateProvider from '../models/IMailTemplateProvider';
 
 export default class HandleBarsMailTemplateProvider
   implements IParseMailTemplateProvider {
   public async parser({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const parseTemplate = handlebars.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
