7f47e2f9eb8af6df622dba65235d5e98cd49b00e
Ariane Mateus <ariane.ccomp@gmail.com>
2020-05-19 07:45:48 -0300

    feat(appointments): :sparkles: list of available days

--- gobarber-backend/src/modules/appointments/services/ListMonthAvailabilityService.ts
+++ gobarber-backend/src/modules/appointments/services/ListMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -32,7 +33,25 @@ export default class ListMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
-    return [{ day: 1, available: false }];
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+    const eachDayArray = Array.from(
+      {
+        length: numberOfDaysInMonth,
+      },
+      (value, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map((day) => {
+      const appointmentsInDay = appointments.filter((appointment) => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
