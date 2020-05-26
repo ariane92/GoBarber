d3129ebab5ccb8ef30601b73d9e51632ed96d52a
Ariane Mateus <ariane.ccomp@gmail.com>
2020-05-26 09:45:25 -0300

    feat: Config Amazon SES

--- gobarber-backend/src/shared/container/providers/index.ts
+++ gobarber-backend/src/shared/container/providers/index.ts
@@ -1,10 +1,13 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
+import Mail from 'nodemailer/lib/mailer';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvides';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateprovider/models/IMailTemplateProvider';
 import HandleBarsMailTemplateProvider from './MailTemplateprovider/implementations/HandleBarsMailTemplateProvider';
@@ -21,5 +24,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
