d6c670d7fd76b829fad57ed361bd8187e301d553
Ariane Mateus <ariane.ccomp@gmail.com>
2020-05-26 10:18:13 -0300

    refactor: Providers email and storage

--- gobarber-backend/src/shared/container/providers/index.ts
+++ gobarber-backend/src/shared/container/providers/index.ts
@@ -1,30 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import Mail from 'nodemailer/lib/mailer';
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvides';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-import IMailTemplateProvider from './MailTemplateprovider/models/IMailTemplateProvider';
-import HandleBarsMailTemplateProvider from './MailTemplateprovider/implementations/HandleBarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandleBarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './StorageProvider/index';
+import './MailTemplateprovider/index';
+import './MailProvider/index';
