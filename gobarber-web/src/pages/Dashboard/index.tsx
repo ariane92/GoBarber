20baf49090be0a57a41730bb20105c9089ab6d52
Ariane Mateus <ariane.ccomp@gmail.com>
2020-05-30 16:57:03 -0300

    feat: Next appointment

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/AuthContext';
@@ -26,6 +35,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 30</span>
+            <span>Sabádo</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento á seguir</strong>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C4D03AQFXemYjJXC_Rw/profile-displayphoto-shrink_100_100/0?e=1596067200&v=beta&t=2DL0dbVIn9TYFtyCxryumdkUbCsfrYZfRU-bQ_godzY"
+                alt="Ariane Mateus"
+              />
+              <strong>Ariane Mateus</strong>
+              <span>
+                {' '}
+                <FiClock /> 10:00{' '}
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
