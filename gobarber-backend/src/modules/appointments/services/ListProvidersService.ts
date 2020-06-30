3f9b105c301aa2b76fe3793bcbc24f4f2bde8a9f
Ariane Mateus <ariane.ccomp@gmail.com>
2020-06-30 10:30:35 -0300

    fix: api adjustment

--- gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-
+import { classToClass } from 'class-transformer';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUserRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -28,7 +28,10 @@ export default class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
